2032a4103ff3cc6bc8e13e118b3000cb8e6da857
Winnie Xiao <winniex@stanford.edu>
2020-11-19 23:29:59 +0100

    while to find the kernal proc, then print process_id & proc_name of process result returned

--- fig_ps.cpp
+++ fig_ps.cpp
@@ -7,20 +7,23 @@ using namespace std;
 int main() {
   printf("Hello world");
   struct kinfo_proc *proc = NULL;
-  
+  size_t size = 0; 
   int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0 };
   int mib_len = 4; 
-  int ret = sysctl (mib, mib_len, NULL, NULL, NULL, 0);
+  int ret = sysctl (mib, mib_len, NULL, &size, NULL, 0);
   if (ret == -1) {
     // errno is set to indicate the error too
     cout << "errno: " << errno << endl;
     perror("sysctl");
   } 
-  
 
-  cout << "ret " << ret << endl;
-  cout << "mib " << *mib << endl; 
- 
+  int proc_len = 
+  for (int i = 0; i < 10; i++) {
+    cout << "ret " << ret << endl;
+    cout << "mib " << *mib << endl; 
+    ret = sysctl (mib, mib_len, NULL, NULL, NULL, 0);
+  }
+  
 
   return 0;
 }
