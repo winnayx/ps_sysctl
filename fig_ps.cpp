e88c0a7aa751093e7e700cd9ac8721aaac1bd112
Winnie Xiao <winniex@stanford.edu>
2020-11-20 00:57:29 +0100

    initial functional version

--- fig_ps.cpp
+++ fig_ps.cpp
@@ -1,29 +1,38 @@
 #include <iostream>
-#include <sys/types.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <sys/sysctl.h>
 
 using namespace std;
 
 int main() {
-  printf("Hello world");
-  struct kinfo_proc *proc = NULL;
-  size_t size = 0; 
+  struct kinfo_proc *res = NULL;
+  size_t res_size = 0;
+ 
   int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0 };
   int mib_len = 4; 
-  int ret = sysctl (mib, mib_len, NULL, &size, NULL, 0);
-  if (ret == -1) {
-    // errno is set to indicate the error too
-    cout << "errno: " << errno << endl;
-    perror("sysctl");
-  } 
 
-  int proc_len = 
-  for (int i = 0; i < 10; i++) {
-    cout << "ret " << ret << endl;
-    cout << "mib " << *mib << endl; 
-    ret = sysctl (mib, mib_len, NULL, NULL, NULL, 0);
+  int err = sysctl (mib, mib_len, res, &res_size, NULL, 0);
+  if (err == -1) {
+    // errno is set to indicate the error too
+    cout << "errno: " << err << endl;
+    perror("sysctl");
   }
+  cout << "after first sysctl, res_size: " << res_size << endl; 
   
+  res = (struct kinfo_proc *)malloc(res_size);
+  err = sysctl (mib, mib_len, res, &res_size, NULL, 0);
+   
+  cout << "after second sysctl, res_size: " << res_size << endl; 
+  int num_res = res_size / sizeof(struct kinfo_proc);
+
+  for (int i = 0; i < num_res; i++) {
+    cout << "pid: " << res[i].kp_proc.p_pid << " cmd" << res[i].kp_proc.p_comm<<  endl;
+  } 
 
   return 0;
 }
+
