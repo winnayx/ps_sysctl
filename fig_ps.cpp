76139a32dd3adaca7b6ebc45e264b58fdf2963a5
Winnie Xiao <winniex@stanford.edu>
2020-11-20 16:40:17 +0100

    working ps

--- fig_ps.cpp
+++ fig_ps.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include <assert.h>
-#include <errno.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 #include <sys/sysctl.h>
 
 using namespace std;
@@ -11,28 +7,37 @@ using namespace std;
 int main() {
   struct kinfo_proc *res = NULL;
   size_t res_size = 0;
- 
+  
   int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0 };
   int mib_len = 4; 
 
-  int err = sysctl (mib, mib_len, res, &res_size, NULL, 0);
-  if (err == -1) {
-    // errno is set to indicate the error too
-    cout << "errno: " << err << endl;
+  if (sysctl(mib, mib_len, NULL, &res_size, NULL, 0) == -1) {
     perror("sysctl");
   }
-  cout << "after first sysctl, res_size: " << res_size << endl; 
   
-  res = (struct kinfo_proc *)malloc(res_size);
-  err = sysctl (mib, mib_len, res, &res_size, NULL, 0);
-   
-  cout << "after second sysctl, res_size: " << res_size << endl; 
-  int num_res = res_size / sizeof(struct kinfo_proc);
-
-  for (int i = 0; i < num_res; i++) {
-    cout << "pid: " << res[i].kp_proc.p_pid << " cmd" << res[i].kp_proc.p_comm<<  endl;
+  if (!(res = (struct kinfo_proc *)malloc(res_size))) {
+    perror("malloc");
   } 
 
+  if (sysctl (mib, mib_len, res, &res_size, NULL, 0) == -1) {
+    free(res);
+    perror("sysctl");
+  }
+   
+  int res_count = res_size / sizeof(struct kinfo_proc);
+  int tty_offset = 0;
+
+  for (int i = res_count - 1; i >= 0; i--) {
+    struct extern_proc curr = res[i].kp_proc;
+    if (res[i].kp_eproc.e_tdev > 0 && strncmp(curr.p_comm, "login", 5)) {
+      if (!tty_offset) tty_offset = res[i].kp_eproc.e_tdev;  
+      cout << "PID: " << curr.p_pid 
+	<< "  TTY: ttys00"<< res[i].kp_eproc.e_tdev - tty_offset
+	<< "  CMD: " << curr.p_comm << endl;
+    } 
+  }
+
+  free(res);
   return 0;
 }
 
